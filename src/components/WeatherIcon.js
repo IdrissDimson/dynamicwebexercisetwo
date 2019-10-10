fa0936363977a3639e8a9930f283369c61d6e708
Idriss Dimson <idriss.dimson@gmail.com>
2019-10-10 15:23:11 -0400

    added more icons

--- src/components/WeatherIcon.js
+++ src/components/WeatherIcon.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCloudRain, faCloud, faSun, faWind } from '@fortawesome/free-solid-svg-icons';
+import { faCloudRain, faCloud, faSun, faWind, faWater,faSnowflake, faCloudShowersHeavy} from '@fortawesome/free-solid-svg-icons';
 
 export default function WeatherIcon({weatherType}) {
     switch(weatherType){
-        case 'Rain':
+        case 'Drizzle':
             return <FontAwesomeIcon icon={faCloudRain} size="6x"/> 
         case 'Clouds':
             return <FontAwesomeIcon icon={faCloud} size="6x"/>
@@ -13,6 +13,12 @@ export default function WeatherIcon({weatherType}) {
             return <FontAwesomeIcon icon={faSun} size="6x"/>
         case 'Windy':
             return <FontAwesomeIcon icon={faWind} size="6x"/>
+        case 'Mist':
+            return <FontAwesomeIcon icon={faWater} size="6x"/>
+        case 'Snow':
+            return <FontAwesomeIcon icon={faSnowflake} size="6x"/>
+        case 'Rain':
+            return <FontAwesomeIcon icon={faCloudShowersHeavy} size="6x"/>
         default:
             return <div>{weatherType}</div>
     }
